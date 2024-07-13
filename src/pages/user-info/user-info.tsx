ca31fd6933fcfb1cf70dd07864c566464c09ca55
cvccvc40 <ebgs3534@gmail.com>
2024-07-13 17:56:41 +0300

    Fixed some errors in user-info page #6

--- src/pages/user-info/user-info.tsx
+++ src/pages/user-info/user-info.tsx
@@ -132,68 +132,56 @@ const UserInfo: FC<IUserInfo> = ({ token, t }) => {
             ) : (
                 <div className='flex flex-col'>
                     <h1 className='text-xl font-semibold mb-5'>{t('about-user-title')}</h1>
-                    <div className='flex flex-col gap-4'>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-id')}</h4>
-                            <p className='text-base'>{user.id}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-role-id')}</h4>
-                            <p className='text-base'>{user.role.id}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-role-title')}</h4>
-                            <p className='text-base'>{user.role.title}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-image-url')}</h4>
-                            <p className='text-base'>{user.image}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-public-nickname')}</h4>
-                            <p className='text-base'>{user.public_nickname}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-full-name')}</h4>
-                            <p className='text-base'>{user.full_name}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-email')}</h4>
-                            <p className='text-base'>{user.email}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-phone')}</h4>
-                            <p className='text-base'>{user.phone}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-github')}</h4>
-                            <p className='text-base'>{user.github}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-payment-info')}</h4>
-                            <p className='text-base'>{user.payment_info}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-tg')}</h4>
-                            <p className='text-base'>{user.tg}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-team-id')}</h4>
-                            <p className='text-base'>{user.team.id}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-team-title')}</h4>
-                            <p className='text-base'>{user.team.title}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-project-id')}</h4>
-                            <p className='text-base'>{user.projects.id}</p>
-                        </div>
-                        <div className='flex flex-row gap-2'>
-                            <h4 className='text-base font-semibold'>{t('user-project-title')}</h4>
-                            <p className='text-base'>{user.projects.title}</p>
-                        </div>
-                    </div>
+                    <Form className='flex flex-col gap-4' >
+                        <Form.Item label={t('user-id')} name='id'>
+                            <Input className='w-64' type="number" value={user.id} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-role-id')} name='role_id'>
+                            <Input className='w-64' type="number" value={user.role.id} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-role-title')} name='role_title'>
+                            <Input className='w-64' type="text" value={user.role.title} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-image-url')} name='image'>
+                            <Input className='w-64' type="text" value={user.image} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-public-nickname')} name='public_nickname' >
+                            <Input className='w-64' type="text" value={user.public_nickname} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-full-name')} name='full_name'>
+                            <Input className='w-64' type="text" value={user.full_name} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-email')} name='email'>
+                            <Input className='w-64' type="text" value={user.email} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-phone')} name='phone'>
+                            <Input className='w-64' type="number" value={user.email} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-github')} name='github'>
+                            <Input className='w-64' type="text" value={user.github} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-payment-info')} name='payment_info'>
+                            <Input className='w-64' type="text" value={user.payment_info} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-tg')} name='tg'>
+                            <Input className='w-64' type="text" value={user.tg} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-team-id')} name='team_id'>
+                            <Input className='w-64' type="number" value={user.team.id} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-team-title')} name='team_title'>
+                            <Input className='w-64' type="text" value={user.team.title} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-project-id')} name='project_id'>
+                            <Input className='w-64' type="number" value={user.projects.id} disabled={true} />
+                        </Form.Item>
+                        <Form.Item label={t('user-project-title')} name='project_title'>
+                            <Input className='w-64' type="text" value={user.projects.title} disabled={true} />
+                        </Form.Item>
+                        <Button className='flex justify-center items-center text-lg w-28 mt-5' htmlType='submit'>
+                            <h4 className='p-2'>{t('changed-data')}</h4>
+                        </Button>
+                    </Form>
                 </div>
             )
             }
