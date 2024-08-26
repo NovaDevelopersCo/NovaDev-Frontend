fa8584602cc203383312892968362a4cff516d25
Zoom <egor147536987@gmail.com>
2024-08-26 15:23:26 +0300

    "Removed Upload component and related code, removed unused imports and variables, and updated form submission to redirect to /roles instead of /admins"

--- src/pages/FRoles/add-role/add-role.tsx
+++ src/pages/FRoles/add-role/add-role.tsx
@@ -1,12 +1,10 @@
 import React, { FC, useContext } from 'react'
 import { useHistory } from 'react-router-dom'
-import { TAdmin, TRest } from '../../../utils/typesFromBackend'
-import { Form, Input, Button, Modal, Upload } from 'antd'
+import { TAdmin } from '../../../utils/typesFromBackend'
+import { Form, Input, Button, Modal } from 'antd'
 import { NotificationContext } from '../../../components/notification-provider/notification-provider'
 import * as roleAPI from '../../../utils/api/role-api'
-import * as userAPI from '../../../utils/api/task-api'
 import clsx from 'clsx'
-import { UploadOutlined } from '@ant-design/icons'
 
 interface IAddRole {
   pathRest: string
@@ -25,11 +23,9 @@ const AddRole: FC<IAddRole> = ({ token, pathRest, t, dark }) => {
   }
   const theme = clsx(dark ? 'black' : 'white')
   const [isModalVisible, setIsModalVisible] = React.useState(false)
-  const [PathRest, setPathRest] = React.useState<{ [key: string]: string }>({})
   const [, setCategory] = React.useState('')
   const [, setTitle] = React.useState('')
   const [, setContent] = React.useState('')
-  const [, setImage] = React.useState('')
 
   function handleChangeContent(e: React.ChangeEvent<HTMLInputElement>): void {
     setContent(e.target.value)
@@ -43,43 +39,21 @@ const AddRole: FC<IAddRole> = ({ token, pathRest, t, dark }) => {
     setTitle(e.target.value)
   }
 
-  function handleChangeImage(info: any): void {
-    if (info.file.status === 'done') {
-      setImage(info.file.originFileObj)
-    }
-  }
-
   const validateMessages = {
     // eslint-disable-next-line no-template-curly-in-string
     required: '${label} ' + `${t('it-is-necessary-to-fill-in')}!`
   }
-  React.useEffect(() => {
-    userAPI
-      .getTasks(token, 1)
-      .then((res) => {
-        const nameRests: { [key: string]: string } = {}
-        res.rests.forEach((rest: TRest) => {
-          if (!nameRests[rest.titleRest] && rest.titleRest) {
-            nameRests[rest.titleRest] = rest._id
-          }
-        })
-        setPathRest(nameRests)
-      })
-      .catch((e) => openNotification(e, 'topRight'))
-  }, [])
 
   const onFinish = (values: any): void => {
     const newLanguageRest: any = {
       nickname: values.nickname,
       password: values.password,
-      level_access: Number(values.level_access),
-      rest_id: PathRest[values.restaurant],
-      image: values.image
+      level_access: Number(values.level_access)
     }
     roleAPI
       .createRole(token, newLanguageRest)
       .then((res: TAdmin) => {
-        history.push(`/${pathRest}/admins`)
+        history.push(`/${pathRest}/roles`)
       })
       .catch((e) => openNotification(e, 'topRight'))
   }
@@ -146,16 +120,6 @@ const AddRole: FC<IAddRole> = ({ token, pathRest, t, dark }) => {
           name='category'
         >
           <Input onChange={handleChangeCategory} />
-        </Form.Item>
-        <Form.Item label={t('image')} name='image'>
-          <Upload
-            name='image'
-            listType='picture'
-            beforeUpload={() => false}
-            onChange={handleChangeImage}
-          >
-            <Button icon={<UploadOutlined />}>{t('upload-photo')}</Button>
-          </Upload>
         </Form.Item>
         <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 4 }}>
           <Button type='primary' htmlType='submit'>
