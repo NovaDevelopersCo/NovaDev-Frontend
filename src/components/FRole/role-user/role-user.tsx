e2c3dcba4f23539541c633995f554afe856fa15e
Zoom <egor147536987@gmail.com>
2024-08-26 19:17:18 +0300

    "Refactor RoleUser component: replaced Form with Input and Button with Select, Table, and Popconfirm; added useEffect hooks for data fetching and updating; modified layout and validation messages."

--- src/components/FRole/role-user/role-user.tsx
+++ src/components/FRole/role-user/role-user.tsx
@@ -1,9 +1,11 @@
-import { Form, Button, Input } from 'antd'
+import { Form, Button, Select, Table, Popconfirm } from 'antd'
 import React, { FC, useContext } from 'react'
-import { useHistory, useLocation, useRouteMatch } from 'react-router-dom'
+import { Link, useLocation, useRouteMatch } from 'react-router-dom'
 import { TAdmin } from '../../../utils/typesFromBackend'
 import * as adminAPI from '../../../utils/api/category-api'
 import { NotificationContext } from '../../notification-provider/notification-provider'
+import { DeleteTwoTone } from '@ant-design/icons'
+import { ColumnsType } from 'antd/es/table'
 
 interface IGroupModifiersForDish {
   pathRest: string
@@ -11,122 +13,169 @@ interface IGroupModifiersForDish {
   t: (arg0: string) => string
 }
 
+interface ILevelsAccess {
+  text: string
+  value: string
+}
+
 const RoleUser: FC<IGroupModifiersForDish> = ({ token, pathRest, t }) => {
   const { openNotification } = useContext(NotificationContext)
-  const [form] = Form.useForm()
-  const history = useHistory()
-  const layout = {
-    labelCol: { span: 4 },
-    wrapperCol: { span: 14 }
-  }
-  // eslint-disable-next-line prefer-regex-literals
-  // const { restId } = useParams<{ restId: string }>()
   const pathname = useLocation().pathname
   const match = useRouteMatch(pathname)
   const restId = Object.keys(match?.params as string)[0]
-  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  const [admin, setAdmin] = React.useState<TAdmin>({} as TAdmin)
-  const [formData, setFormData] = React.useState(() => {
-    const storedFormDataString = localStorage.getItem('formDataAdmin')
-    return storedFormDataString ? JSON.parse(storedFormDataString) : null
-  })
-
-  const handleFormChange = (): void => {
-    const allValues = form.getFieldsValue()
-    const updateallValues = { ...allValues, _id: admin._id }
-    setFormData(updateallValues)
-  }
-
-  React.useEffect(() => {
-    if (Object.keys(admin).length > 0 && formData) {
-      if (admin._id !== formData._id) {
-        localStorage.removeItem('formDataAdmin')
-      }
-    }
-    localStorage.setItem('formDataAdmin', JSON.stringify(formData))
-  }, [formData])
+  const [data, setData] = React.useState<TAdmin[]>([])
+  const [unUsedAdmins, setUnUsedAdmins] = React.useState<TAdmin[]>([])
+  const [levelsAccess, setLevelsAccess] = React.useState<ILevelsAccess[]>([])
+  const [update, setUpdate] = React.useState<boolean>(true)
+  const location = useLocation()
 
   React.useEffect(() => {
     adminAPI
-      .getAdmin(token, restId)
-      .then((res: TAdmin) => {
-        setAdmin(res)
+      .getAllCategories()
+      .then((res) => {
+        const nameRests: TAdmin[] = []
+        const unUsedAdmied: TAdmin[] = []
+        res.admins.forEach((admin: TAdmin) => {
+          if (admin.rest_id === restId) {
+            nameRests.push(admin)
+          } else if (!admin.rest_id) unUsedAdmied.push(admin)
+        })
+        setUnUsedAdmins(unUsedAdmied)
+        setData(nameRests)
       })
       .catch((e) => openNotification(e, 'topRight'))
-  }, [])
+    const currentPath = location.pathname
+    window.localStorage.setItem('initialRoute', currentPath)
+  }, [restId, update])
 
   React.useEffect(() => {
-    const storedFormDataString = localStorage.getItem('formDataAdmin')
-    const parsedFormData = storedFormDataString
-      ? JSON.parse(storedFormDataString)
-      : null
-    if (parsedFormData && parsedFormData._id === admin._id) {
-      form.setFieldsValue({
-        password: parsedFormData.password
-      })
+    const levelsAccessNames: { [key: string]: boolean } = {}
+    const resultArrayLevels: ILevelsAccess[] = []
+    data.forEach((admin: TAdmin) => {
+      if (!levelsAccessNames[admin.level_access]) {
+        levelsAccessNames[admin.level_access] = true
+      }
+    })
+    for (const key of Object.keys(levelsAccessNames)) {
+      resultArrayLevels.push({ text: key, value: key })
     }
-  }, [admin])
+    setLevelsAccess(resultArrayLevels)
+  }, [data])
+  function handleDeleteModifierFromDish(values: any): void {
+    const newLanguageRest: any = {
+      _id: values._id,
+      nickname: values.nickname,
+      password: values.password,
+      level_access: Number(values.level_access),
+      rest_id: ''
+    }
+    adminAPI
+      .updateAdmin(token, newLanguageRest)
+      .then((res: any) => {
+        setUpdate(!update)
+      })
+      .catch((e) => openNotification(e, 'topRight'))
+  }
+
+  const columns: ColumnsType<TAdmin> = [
+    {
+      title: `${t('login')}`,
+      dataIndex: 'nickname',
+      key: 'nickname',
+      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+      render: (nickname, admin) => (
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        <Link to={`/${pathRest}/admin/:${admin._id}`}>{nickname}</Link>
+      )
+    },
+    {
+      title: `${t('level_access')}`,
+      dataIndex: 'level_access',
+      key: 'level_access',
+      render: (levelAccess) => <p>{levelAccess}</p>,
+      sorter: (a, b) => a.level_access - b.level_access,
+      filters: [...levelsAccess],
+      onFilter: (value: string | number | boolean, record) =>
+        // eslint-disable-next-line eqeqeq
+        record.level_access == value
+    },
+    {
+      title: '',
+      dataIndex: 'operation',
+      render: (_, record: { _id: React.Key }) =>
+        data.length >= 1 ? (
+          <Popconfirm
+            title={t('delete-admin-from-restaurant')}
+            onConfirm={() => handleDeleteModifierFromDish(record)}
+          >
+            <DeleteTwoTone />
+          </Popconfirm>
+        ) : null
+    }
+  ]
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 14 }
+  }
+  const [form] = Form.useForm()
+
+  const onFinish = (values: any): void => {
+    adminAPI
+      .getAdmin(token, values._id)
+      .then((res: TAdmin) => {
+        const newLanguageRest: any = {
+          _id: res._id,
+          nickname: res.nickname,
+          password: res.password,
+          level_access: Number(res.level_access),
+          rest_id: restId
+        }
+        adminAPI
+          .updateAdmin(token, newLanguageRest)
+          .then((res: any) => {
+            setUpdate(!update)
+          })
+          .catch((e) => openNotification(e, 'topRight'))
+      })
+      .catch((e) => openNotification(e, 'topRight'))
+  }
+
   const validateMessages = {
     // eslint-disable-next-line no-template-curly-in-string
-    required: '${label} ' + `${t('it-is-necessary-to-fill-in')}!`
+    required: t('you-nedd-choose-group-modifier-for-dish')
   }
-  const onFinish = (values: any): void => {
-    const passwordAdmin: any = {
-      _id: admin._id,
-      newPassword: values.password
-    }
-    adminAPI
-      .updateAdminPassword(token, passwordAdmin)
-      .then((res: TAdmin) => {
-        localStorage.removeItem('formDataAdmin')
-        history.push(`/${pathRest}/admins`)
-      })
-      .catch((e) => openNotification(e, 'topRight'))
-  }
+
   return (
-    <Form
-      {...layout}
-      onFinish={onFinish}
-      validateMessages={validateMessages}
-      name='admin'
-      form={form}
-      style={{ paddingTop: '1.5rem' }}
-      onValuesChange={handleFormChange}
-    >
-      <Form.Item
-        label={t('password')}
-        rules={[{ required: true }]}
-        name='password'
+    <>
+      <Form
+        {...layout}
+        onFinish={onFinish}
+        name='dish'
+        form={form}
+        validateMessages={validateMessages}
+        style={{ paddingTop: '1.5rem' }}
       >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item
-        label={t('сonfirm-password')}
-        name='confirm'
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true
-          },
-          ({ getFieldValue }) => ({
-            async validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return await Promise.resolve()
-              }
-              return await Promise.reject(new Error(t('password-do-not-match')))
-            }
-          })
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 4 }}>
-        <Button type='primary' htmlType='submit'>
-          {t('save')}
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label={t('add-admin')}
+          name='_id'
+          rules={[{ required: true }]}
+        >
+          <Select>
+            {unUsedAdmins.map((admin, index) => (
+              <Select.Option value={admin._id} key={index}>
+                {admin.nickname}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+          <Button type='primary' htmlType='submit'>
+            {t('add-admin-to-rest')}
+          </Button>
+        </Form.Item>
+      </Form>
+      <Table columns={columns} dataSource={data} />
+    </>
   )
 }
 export default RoleUser
