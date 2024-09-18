f9db998245fabbb6f7b034d4ac3b7f59646822da
cvccvc40 <ebgs3534@gmail.com>
2024-09-18 21:52:23 +0300

    Deleted image title, fixed image in top, aligned the text

--- src/components/view-user-info/view-user-info.tsx
+++ src/components/view-user-info/view-user-info.tsx
@@ -33,18 +33,14 @@ const ViewUserInfo: FC<IViewUserInfo> = ({ token, t }) => {
                         </div>
                     </div>
                     <div>
-                        <div className='flex items-center justify-center mt-2'>
-                                <h4 className='text-lg font-semibold'>{t('user-profile-image')}</h4>
-                        </div>
                         {isImageError ? (
                                 <div className='flex justify-center mb-2'>
                                     <p className='text-base'>Image preload error</p>
                                 </div>
                         ) : (
-                            <div className='flex gap-2 mb-2'>
-                                <h2>{t('user-image')}</h2>
-                                <div className='flex items-center justify-center'>
-                                    <Image className='rounded-lg overflow-hidden' width={200} height={200} src={user?.info?.image} alt="Profile photo" onError={handleImageError} />
+                            <div className='flex gap-2 mb-2  items-center justify-center'>
+                                <div>
+                                    <Image className='rounded-lg overflow-hidden  border-2 border-black object-cover' width={200} height={200} src={user?.info?.image} alt="Profile photo" onError={handleImageError} />
                                 </div>
                             </div>
                         )
@@ -62,31 +58,31 @@ const ViewUserInfo: FC<IViewUserInfo> = ({ token, t }) => {
                                 <h4 className='text-lg font-semibold'>{t('base-info-title')}</h4>
                             </div>
                             <div className='flex flex-col gap-3 mb-2'>
-                                <div className='flex gap-2'>
+                                <div className='flex gap-2 items-center'>
                                     <h2>{t('user-public-nickname')}</h2>
                                     <p className='text-base'>{user?.info?.public_nickname ?? ''}</p>
                                 </div>
-                                <div className='flex gap-2'>
+                                <div className='flex gap-2 items-center'>
                                     <h2>{t('user-full-name')}</h2>
                                     <p className='text-base'>{user?.info?.full_name ?? ''}</p>
                                 </div>
-                                <div className='flex gap-2'>
+                                <div className='flex gap-2 items-center'>
                                     <h2>{t('user-email')}</h2>
                                     <p className='text-base'>{user?.info?.email ?? ''}</p>
                                 </div>
-                                <div className='flex gap-2'>
+                                <div className='flex gap-2 items-center'>
                                     <h2>{t('user-phone')}</h2>
                                     <p className='text-base'>{user?.info?.phone ?? null}</p>
                                 </div>
-                                <div className='flex gap-2'>
+                                <div className='flex gap-2 items-center'>
                                     <h2>{t('user-github')}</h2>
                                     <p className='text-base'>{user?.info?.github ?? ''}</p>
                                 </div>
-                                <div className='flex gap-2'>
+                                <div className='flex gap-2 items-center'>
                                     <h2>{t('user-payment-info')}</h2>
                                     <p className='text-base'>{user?.info?.payment_info ?? ''}</p>
                                 </div>
-                                <div className='flex gap-2'>
+                                <div className='flex gap-2 items-center'>
                                     <h2>{t('user-tg')}</h2>
                                     <p className='text-base'>{user?.info?.tg ?? ''}</p>
                                 </div>
